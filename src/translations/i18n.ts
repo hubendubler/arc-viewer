d7e3131aad35744a18e77923c36fb159fa03153c
hubendubler <hubendubler@users.noreply.github.com>
2021-08-23 21:07:51 +0200

    Fix syntax in i18n configuration

--- src/translations/i18n.ts
+++ src/translations/i18n.ts
@@ -1,5 +1,5 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 
 import LanguageDetector from 'i18next-browser-languagedetector';
 
@@ -14,12 +14,12 @@ i18n.use(LanguageDetector)
 
         resources: {
             de,
-            en
+            en,
         },
 
         interpolation: {
-            escapeValue: false
-        }
+            escapeValue: false,
+        },
     })
     .catch((error) => {
         console.warn('Could not initialise translation services:', error);
