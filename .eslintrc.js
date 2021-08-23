2a1e09d1d60ee6a1eabba754bb49f02a4d32f83f
hubendubler <hubendubler@users.noreply.github.com>
2021-08-23 13:31:38 +0200

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+        'unicorn'
+    ],
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        project: './tsconfig.json',
+        ecmaFeatures: {
+            jsx: true
+        },
+        tsconfigRootDir: __dirname,
+    },
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb',
+        'airbnb/hooks',
+        'airbnb-typescript',
+        'plugin:unicorn/recommended',
+        'prettier'
+    ],
+    rules: {
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'import/prefer-default-export': 'off',
+        'import/no-default-export': 'error',
+        'unicorn/filename-case': [
+            "error",
+            {
+                "cases": {
+                    "camelCase": true,
+                    "pascalCase": true,
+                },
+                "ignore": [
+                    'react-app-env.d.ts'
+                ]
+            }
+        ],
+        'unicorn/prevent-abbreviations': [
+            "error",
+            {
+                checkFilenames: false
+            }
+        ],
+    }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    tabWidth: 4,
+    singleQuote: true,
+    bracketSpacing: false,
+    arrowParens: "always",
+    endOfLine: "lf",
+    printWidth: 80,
+}
